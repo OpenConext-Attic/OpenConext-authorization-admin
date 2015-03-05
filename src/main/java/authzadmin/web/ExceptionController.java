dbad61ee646e20e479aec5a3ce6026a2eb09929f
Lars Vonk <lars.vonk@gmail.com>
2015-03-05 09:41:40 +0100

    Boyscout: remove unused import.

--- src/main/java/authzadmin/web/ExceptionController.java
+++ src/main/java/authzadmin/web/ExceptionController.java
@@ -3,7 +3,6 @@ package authzadmin.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
-import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
