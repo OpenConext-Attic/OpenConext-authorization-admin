cdfc5665f22829a12d61b3c130c2e7f356b18ad0
oharsta <oharsta@zilverline.com>
2015-04-24 13:01:18 +0200

    When leaving the non-empty scope field add the scope

--- src/main/resources/static/javascripts/admin.js
+++ src/main/resources/static/javascripts/admin.js
@@ -1,34 +1,46 @@
-$(function() {
+$(function () {
 
-  $("#scope-name").on("keyup", function(e) {
+  function addScope(self) {
+    self.before("<span class='tag c-button' data-value='" + self.val() + "'><span>" + self.val() + "</span><a href='#'>x</a></span>")
+    $("#scopes").append("<option selected='selected' value='" + self.val() + "'>" + self.val() + "</option>");
+    self.val("");
+  }
+
+  $("#scope-name").on("keyup", function (e) {
     var self = $(this);
-    if(e.which == 13) {
-      self.before("<span class='tag c-button' data-value='" + self.val() + "'><span>" + self.val() + "</span><a href='#'>x</a></span>")
-      $("#scopes").append("<option selected='selected' value='" + self.val() + "'>" + self.val() + "</option>");
-      self.val("");
+    if (e.which == 13) {
+      addScope(self);
     }
     return false;
   });
 
-  $("#scope-name").on("keypress", function(e) {
-    if(e.which == 13) {
+  $("#scope-name").on("blur", function (e) {
+    var self = $(this);
+    if (self.val()) {
+      addScope(self);
+    }
+    return false;
+  });
+
+  $("#scope-name").on("keypress", function (e) {
+    if (e.which == 13) {
       // Stop form submit by returning false
       return false;
     }
   });
 
-  $(document).on("click", ".tag a", function(e) {
+  $(document).on("click", ".tag a", function (e) {
     var self = $(this);
     var optionValue = self.parent().data("value");
-    $("#scopes option[value='" + optionValue+ "']").remove();
+    $("#scopes option[value='" + optionValue + "']").remove();
     self.parent().remove();
   });
 
-  $(".flash-notice a").on("click", function() {
+  $(".flash-notice a").on("click", function () {
     $(".flash-notice").toggleClass("hidden");
   });
 
-  $("form.delete-client").on("submit", function(e) {
+  $("form.delete-client").on("submit", function (e) {
     var clientId = $(this).data("client");
     return confirm("Are you sure you want to delete client: " + clientId + "?");
   });
