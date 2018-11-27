ede6394eac625398f8ab3689a687e8d6a690f7da
Okke Harsta <oharsta@zilverline.com>
2018-11-27 09:41:39 +0100

    Convert headers values to UTF-8

--- src/main/java/authzadmin/shibboleth/ShibbolethPreAuthenticatedProcessingFilter.java
+++ src/main/java/authzadmin/shibboleth/ShibbolethPreAuthenticatedProcessingFilter.java
@@ -4,8 +4,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.util.Optional;
 
 public class ShibbolethPreAuthenticatedProcessingFilter extends AbstractPreAuthenticatedProcessingFilter {
@@ -20,10 +22,11 @@ public class ShibbolethPreAuthenticatedProcessingFilter extends AbstractPreAuthe
 
   @Override
   protected Object getPreAuthenticatedPrincipal(final HttpServletRequest request) {
-    Optional<String> uid = Optional.of(request.getHeader(UID_HEADER_NAME));
-    Optional<String> displayName = Optional.of(request.getHeader(DISPLAY_NAME_HEADER_NAME));
+    Optional<String> uid = Optional.of(getHeader(UID_HEADER_NAME, request));
+    Optional<String> displayName = Optional.of(getHeader(DISPLAY_NAME_HEADER_NAME, request));
     if (!uid.isPresent() || !displayName.isPresent()) {
-      throw new PreAuthenticatedCredentialsNotFoundException("Missing header information");
+      throw new PreAuthenticatedCredentialsNotFoundException(
+        String.format("Missing header information. Required headers are: %s and %s", UID_HEADER_NAME, DISPLAY_NAME_HEADER_NAME));
     }
     return new FederatedUser(uid.get(), displayName.get(), AuthorityUtils.createAuthorityList("USER"));
   }
@@ -32,4 +35,15 @@ public class ShibbolethPreAuthenticatedProcessingFilter extends AbstractPreAuthe
   protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
     return "N/A";
   }
+
+  private String getHeader(String name, HttpServletRequest request) {
+    String header = request.getHeader(name);
+    try {
+      return StringUtils.hasText(header) ?
+        new String(header.getBytes("ISO8859-1"), "UTF-8") : header;
+    } catch (UnsupportedEncodingException e) {
+      throw new IllegalArgumentException(e);
+    }
+  }
+
 }
